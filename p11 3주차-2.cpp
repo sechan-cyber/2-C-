88e4dc4914310c6e5a4a233d06a5068dcdf6266e
Sechan <jinzahaza77@gmail.com>
2026-09-22 15:30:08 +0900

    Add files via upload

---
+++ p11 3주차-2.cpp
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+void print_array(int arr[], int size);
+
+int main()
+{
+
+}
+
+void print_array(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
